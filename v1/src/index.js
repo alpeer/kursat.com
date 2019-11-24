04603afe512b6cb84f623d8a926c46d56cf934ee
Ömer Gülçiçek <iletisim@omergulcicek.com>
2019-11-24 17:08:46 +0300

    Eski versiyonlar tek klasör altında toplandı.

--- v1/src/index.js
+++
@@ -1,5 +0,0 @@
-import React                        from "react";
-import ReactDOM                     from "react-dom";
-import OmerGulcicek                 from "./OmerGulcicek";
-
-ReactDOM.render(<OmerGulcicek />, document.getElementById("omergulcicek"));
